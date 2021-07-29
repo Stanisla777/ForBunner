1bb815a30ec0414de3788bfbc1021ae9a88d368b
Станислав <stanisla777@mail.ru>
2021-07-29 14:02:56 +0300

    Первая страница

--- dist/js/app.js
+++ dist/js/app.js
@@ -9,4 +9,28 @@ $(document).ready(function () {
     $('.modal').hide();
     $('#lean_overlay').hide();
   });
+  $('.modal__form form').validate({
+    rules: {
+      name: {
+        required: true
+      },
+      number: {
+        required: true
+      },
+      check: {
+        required: true
+      }
+    },
+    messages: {
+      name: {
+        required: "Это поле обязательно для заполнения"
+      },
+      number: {
+        required: "Это поле обязательно для заполнения"
+      },
+      check: {
+        required: ""
+      }
+    }
+  });
 });
--- src/js/app.js
+++ src/js/app.js
@@ -14,4 +14,31 @@ $(document).ready(function () {
     $('#lean_overlay').hide();
   });
 
+  $('.modal__form form').validate({
+    rules:{
+      name: {
+        required: true,
+      },
+      number: {
+        required: true
+      },
+      check: {
+        required: true
+      },
+
+    },
+    messages: {
+      name: {
+        required: "Это поле обязательно для заполнения",
+      },
+      number: {
+        required: "Это поле обязательно для заполнения",
+      },
+      check: {
+        required: "",
+      }
+
+    }
+  })
+
 })
